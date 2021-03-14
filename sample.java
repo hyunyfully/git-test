fd0194cd7b203f036063ec08d98321dfce91496e
DESKTOP-NBC7N82\ffiwe <ffiwe@naver.com>
2021-03-14 23:01:28 +0900

    change branch add sysout

--- sample.java
+++ sample.java
@@ -1,5 +1,7 @@
 public class sample{
     public static void main(String[] strings){
         System.out.println("First Class for Git-test Repository");
+
+        System.out.println("로컬에 branch1을 생성");
     }
 }
